b481c335427878d375689e2bce55b443d5b21e6e
ArpanSankesh <arpansankesh830@gmail.com>
2024-06-17 21:55:30 +0530

    Cursor

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ let main = document.querySelector('#main');
 let cursor = document.querySelector('#cursor');
 
 let move = (delts) => {
+    cursor.style.display = 'block';
     cursor.style.top = delts.clientY + "px";
     cursor.style.left = delts.clientX + "px";
-    // console.log(delts.clientY);
-    // console.log(delts.clientX);
+    cursor.style.width = 15 + "px";
+    cursor.style.height = 15 + "px";
 }
 
-main.addEventListener('mousemove', move)
+let hideCursor = () => {
+    cursor.style.display = 'none';
+    cursor.style.width = 0 + "px";
+    cursor.style.height = 0 + "px";
+};
+
+
+main.addEventListener('mousemove', move);
+main.addEventListener('mouseleave', hideCursor);
